36bb55074882f73f479a0d3f19ad76bbc0fc6f75
Enes Aktay <>
2021-09-23 00:57:48 +0200

    update page with turkish page

--- assets/js/init.js
+++ assets/js/init.js
@@ -187,6 +187,9 @@ $(document).ready(function(){
 
         // Update the count down every 1 second
         var x = setInterval(function() {
+            if($("#countdown").length == 0) {
+                return;
+            }
 
         // Get today's date and time
         var now = new Date().getTime();
@@ -201,14 +204,45 @@ $(document).ready(function(){
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         // Display the result in the element with id="demo"
-        document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
+        document.getElementById("countdown").innerHTML = days + "T " + hours + "Std "
+        + minutes + "Min " + seconds + "Sek ";
 
         // If the count down is finished, write some text
         if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "EXPIRED";
     }
+    }, 1000);
+
+
+    // Update the count down every 1 second
+    var xy = setInterval(function() {
+
+        if($("#countdown-tr").length == 0) {
+            return;
+        }
+
+        // Get today's date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now and the count down date
+        var distance = countDownDate - now;
+
+        // Time calculations for days, hours, minutes and seconds
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        // Display the result in the element with id="demo"
+        document.getElementById("countdown-tr").innerHTML = days + "gn " + hours + "sa "
+            + minutes + "dk " + seconds + "sn ";
+
+        // If the count down is finished, write some text
+        if (distance < 0) {
+            clearInterval(xy);
+            document.getElementById("countdown-tr").innerHTML = "EXPIRED";
+        }
     }, 1000);
 
 });
