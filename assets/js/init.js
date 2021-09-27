73bfeff17b1838a816de0f231c296847635c86ff
Enes Aktay <>
2021-09-27 16:40:35 +0200

    english page

--- assets/js/init.js
+++ assets/js/init.js
@@ -245,6 +245,37 @@ $(document).ready(function(){
         }
     }, 1000);
 
+
+    // Update the count down every 1 second
+    var xyz = setInterval(function() {
+
+      if($("#countdown-en").length == 0) {
+          return;
+      }
+
+      // Get today's date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="demo"
+      document.getElementById("countdown-en").innerHTML = days + "d " + hours + "h "
+          + minutes + "m " + seconds + "s ";
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+          clearInterval(xy);
+          document.getElementById("countdown-en").innerHTML = "EXPIRED";
+      }
+  }, 1000);
+
 });
 
 /*-----------------------------------------------------------------------------------*/
