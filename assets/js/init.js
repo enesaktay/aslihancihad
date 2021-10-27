60bb7333a77da765eac4ddac34bb37665b747fdb
Enes Aktay <>
2021-10-27 09:05:39 +0200

    update ne wdate

--- assets/js/init.js
+++ assets/js/init.js
@@ -183,7 +183,7 @@ $(document).ready(function(){
 
 
         // Set the date we're counting down to
-        var countDownDate = new Date("Oct 10, 2021 17:00:00").getTime();
+        var countDownDate = new Date("Nov 11, 2021 17:00:00").getTime();
 
         // Update the count down every 1 second
         var x = setInterval(function() {
