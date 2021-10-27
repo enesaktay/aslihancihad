f382d69087e4a7d246714b97c40e7f39c7c32018
Enes Aktay <>
2021-10-27 10:58:04 +0200

    wrong date lol

--- assets/js/init.js
+++ assets/js/init.js
@@ -183,7 +183,7 @@ $(document).ready(function(){
 
 
         // Set the date we're counting down to
-        var countDownDate = new Date("Nov 11, 2021 17:00:00").getTime();
+        var countDownDate = new Date("Nov 07, 2021 16:00:00").getTime();
 
         // Update the count down every 1 second
         var x = setInterval(function() {
